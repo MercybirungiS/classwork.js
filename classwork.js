d043247d02156f72c6a2d1c7077c672201b6190f
MercybirungiS <mercyssbirungi@gmail.com>
2021-03-19 11:38:51 +0300

    classwork

---
+++ classwork.js
@@ -0,0 +1,13 @@
+function nameFunction(){
+    var a = " Mercy",  b = "Hope";
+
+    function identity(){
+        var z = a + " and " + b + " are friends "   
+        console.log (z) 
+
+    }
+identity()
+return z
+} 
+nameFunction() 
+return a && b
